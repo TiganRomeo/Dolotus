dbe8883526f9b601bdb0521250b4849a5f899ffc
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-08-28 13:36:03 +0300

    Users v1.4

--- dolotus/apps/users/models.py
+++ dolotus/apps/users/models.py
@@ -2,11 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)  # Mandatory email field
-    phone_number = models.CharField(max_length=20, blank=True)  # Optional phone number
-    street_address = models.CharField(max_length=255, blank=True)  # Optional street address
-    account_name = models.CharField(max_length=255, blank=True)  # Optional account name
-    country = models.CharField(max_length=100, blank=True)  # Optional country
+    # Custom fields for e-commerce user
+    street_address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    
+    # Additional fields for e-commerce user
+    phone_number = models.CharField(max_length=20, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    
+    # Define other fields as needed for your e-commerce user profile
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        app_label = 'users'  # Add this line to specify the app_label
