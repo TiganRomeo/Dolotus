050d07274d987722ab72fdc73dffd15b8ec1756f
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-08-28 14:28:37 +0300

    Header design

--- dolotus/apps/users/urls.py
+++ dolotus/apps/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -6,4 +7,6 @@ app_name = 'users'  # Set an app namespace if desired
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.user_profile, name='user_profile'),
 ]
