80a12b0ece07935ce36cb25d3ca665e74dbb0208
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-09-04 11:34:00 +0300

    Users v2.2

--- dolotus/apps/users/models.py
+++ dolotus/apps/users/models.py
@@ -2,21 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Custom fields for e-commerce user
     street_address = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=100, blank=True)
     state = models.CharField(max_length=100, blank=True)
     postal_code = models.CharField(max_length=20, blank=True)
     country = models.CharField(max_length=100, blank=True)
-    
-    # Additional fields for e-commerce user
     phone_number = models.CharField(max_length=20, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    
-    # Define other fields as needed for your e-commerce user profile
 
     def __str__(self):
         return self.username
-
-    class Meta:
-        app_label = 'users'  # Add this line to specify the app_label
--- dolotus/config/settings.py
+++ dolotus/config/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'apps.users',
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
