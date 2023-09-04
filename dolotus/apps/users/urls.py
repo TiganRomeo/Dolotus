804cfe4f89e63a7ea6b34ac0fc41813f27ad5756
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-09-04 14:32:47 +0300

    Users v2.6

--- dolotus/apps/users/urls.py
+++ dolotus/apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import *
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/', views.user_register, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Add this line
 ]
--- dolotus/apps/users/views.py
+++ dolotus/apps/users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 
 def user_register(request):
     if request.method == 'POST':
@@ -16,3 +17,11 @@ def user_register(request):
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'users/logout.html'  # Optional: Create a custom logout template
+
+    def logout(self, request):
+        # Add your custom logic here
+        messages.success(request, 'You have been logged out successfully.')  # Display a success message
+        return super().logout(request)
